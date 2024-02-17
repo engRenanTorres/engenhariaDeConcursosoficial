912f2295373241852b10312a6586564554547dd6
RenanWindowsMesa <engrtorres@outlook.com>
2024-02-17 19:00:51 -0300

    Frontend updated

---
+++ client-app/.eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'jsx-a11y/aria-role': 0,
+    'prettier/prettier': 1,
+    'require-default-props': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/require-default-props': 'off',
+    'no-use-before-define': [
+      1,
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        allowNamedExports: false,
+      },
+    ],
+  },
+};
---
+++ client-app/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import AppRouter from './routers';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
